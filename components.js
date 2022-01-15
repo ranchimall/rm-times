737b08f5b4e2a98dee1b98c90e6e9a676bbb6b7f
sairaj mote <sairajmote3@gmail.com>
2022-01-16 03:11:21 +0530

    Feature update
    -- added search result page

--- components.js
+++ components.js
@@ -3665,7 +3665,7 @@ customElements.define('tags-input', class extends HTMLElement {
             e.preventDefault()
         }
         if (e.target.value.trim() !== '') {
-            if (e.key === 'Enter' || e.key === ',' || e.key === '/' || e.code === 'Space') {
+            if (e.key === 'Enter' || e.key === ',' || e.key === '/') {
                 const tagValue = e.target.value.trim()
                 if (this.tags.has(tagValue)) {
                     this.tagsWrapper.querySelector(`[data-value="${tagValue}"]`).animate([
